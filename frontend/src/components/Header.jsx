7c8a65ed2645dd7ab9e72a3c18cf316a52359064
Artem Yushko <yushko2002@gmail.com>
2023-12-19 12:05:54 -0600

    organized the frontend structure

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import '../css/header.css';
 
 const Header = () => {
+
+    function handleClick() {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    };
+
     return (
-        <header>
-            <button>About Us</button>
-            <button>Contact</button>
-        </header>
+        <div className='header'>
+            <div className='side'>
+                <p onClick={handleClick}>CarlPics</p>
+            </div>
+            <div className='side'>
+                <p onClick={() => console.log('About Us clicked')}>About Us</p>
+                <p onClick={() => console.log('Contacts clicked')}>Contact</p>
+            </div>
+        </div>
     );
 };
 
