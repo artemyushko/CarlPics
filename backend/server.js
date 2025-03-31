0d317083c5385cd212c01ab7021b4059d9ba0df6
Artem Yushko <yushko2002@gmail.com>
2025-03-31 15:52:08 -0500

    server basics

--- backend/server.js
+++ backend/server.js
@@ -25,4 +25,31 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
+function convertTsvs(req, res) {
+  // create the bare db
+  connection.query('CREATE TABLE images (' +
+                    'd INT NOT NULL PRIMARY KEY,' +
+                    'name VARCHAR(40),' +
+                    'type VARCHAR(10),' +
+                    'owner_id INT NOT NULL,' +
+                    'date_made DATE,' +
+                    'rental_price FLOAT' +
+                    ');', function (error, results, fields) {
+                    // error will be an Error if one occurred during the query
+                    // results will contain the results of the query
+                    // fields will contain information about the returned results fields (if any)
+                  });
+
+  // load the data from the tsv files into the db
+  connection.query('LOAD DATA LOCAL INFILE "images.tsv" ' + 
+                  'INTO TABLE images FIELDS TERMINATED BY \'\\t\' ' + 
+                  'LINES TERMINATED BY \'\\n\' IGNORE 1 LINES ' + 
+                  '(id, name, type, owner_id, @datevar, rental_price);',
+                   function (error, results, fields) {
+                    // error will be an Error if one occurred during the query
+                    // results will contain the results of the query
+                    // fields will contain information about the returned results fields (if any)
+                  });
+}
+
 connection.end();
